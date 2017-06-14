7429ee08fa97b5e6b4c38b48716b601577f90f9e
Ankitkamboj <ankit.kambojsre@gmail.com>
2017-06-14 19:42:13 +0530

    mic commit

--- app/src/main/java/com/innoapps/sadaqah/screens/login/LoginActivity.java
+++ app/src/main/java/com/innoapps/sadaqah/screens/login/LoginActivity.java
@@ -1,15 +1,39 @@
 package com.innoapps.sadaqah.screens.login;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.content.res.Configuration;
 import android.graphics.Typeface;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookAuthorizationException;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.innoapps.sadaqah.R;
 import com.innoapps.sadaqah.screens.login.model.LoginModel;
 import com.innoapps.sadaqah.screens.login.presenter.LoginPresenter;
@@ -21,14 +45,18 @@ import com.innoapps.sadaqah.utils.AppFonts;
 import com.innoapps.sadaqah.utils.CheckPermissions;
 import com.innoapps.sadaqah.utils.Utils;
 
+import org.json.JSONObject;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 import java.util.Locale;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import butterknife.OnClick;
 
-public class LoginActivity extends AppCompatActivity implements LoginView {
-
+public class LoginActivity extends AppCompatActivity implements LoginView, GoogleApiClient.OnConnectionFailedListener {
     @InjectView(R.id.txt_welcome)
     TextView txt_welcome;
     @InjectView(R.id.txt_sign_in)
@@ -57,6 +85,12 @@ public class LoginActivity extends AppCompatActivity implements LoginView {
     RelativeLayout lay_google;
 
     LoginPresenter loginPresenter;
+    private CallbackManager callbackManager;
+    GoogleApiClient mGoogleApiClient;
+
+    private static final int RC_SIGN_IN = 007;
+    private static final String TAG = LoginActivity.class.getSimpleName();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -73,6 +107,58 @@ public class LoginActivity extends AppCompatActivity implements LoginView {
         ButterKnife.inject(this);
         loginPresenter = new LoginPresenterImpl();
         setFonts();
+        // fbkeyhash();
+        FacebookSdk.sdkInitialize(getApplicationContext());
+
+        // Configure sign-in to request the user's ID, email address, and basic
+// profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        // Build a GoogleApiClient with access to the Google Sign-In API and the
+// options specified by gso.
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+
+    }
+
+
+    private void fbkeyhash() {
+
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "com.innoapps.sadaqah", PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.i("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
+        }
+    }
+
+    @OnClick(R.id.lay_fb)
+    public void facebooklogin() {
+        openFacebookLogin();
+
+    }
+
+    @OnClick(R.id.lay_google)
+    public void googleLogin() {
+    openGoogleLogin();
+    }
+
+    private void openGoogleLogin() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+
 
     }
 
@@ -136,6 +222,113 @@ public class LoginActivity extends AppCompatActivity implements LoginView {
     @Override
     public void onLoginInternetError() {
 
+    }
+
+
+    public void openFacebookLogin() {
+        if (AccessToken.getCurrentAccessToken() != null) {
+            LoginManager.getInstance().logOut();
+        }
+        LoginManager.getInstance().logInWithReadPermissions(LoginActivity.this, (Arrays.asList("public_profile", "user_friends", "user_birthday", "user_about_me", "email")));
+        callbackManager = CallbackManager.Factory.create();
+        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d("tag", "FF fb onSuccess");
+                GraphRequest request = GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(JSONObject object, GraphResponse response) {
+                        try {
+                            Log.e("Response", "" + object);
+                            //  ((LoginFragment) loginFragment).onLoginCompleted(object);
+                        } catch (Exception e) {
+                            // TODO Auto-generated catch block
+                            e.printStackTrace();
+                        }
+
+
+                    }
+                });
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id,name,link,birthday,picture,email,gender");
+                request.setParameters(parameters);
+                request.executeAsync();
+
+
+            }
+
+
+            @Override
+            public void onCancel() {
+                Log.d("tag", "fb onCancel");
+                if (AccessToken.getCurrentAccessToken() != null) {
+                    LoginManager.getInstance().logOut();
+                }
+            }
+
+
+            @Override
+            public void onError(FacebookException exception) {
+                if (exception instanceof FacebookAuthorizationException) {
+                    if (AccessToken.getCurrentAccessToken() != null) {
+                        LoginManager.getInstance().logOut();
+                    }
+                }
+            }
+        });
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        try {
+            if (callbackManager != null) {
+                callbackManager.onActivityResult(requestCode, resultCode, data);
+            }
+
+            if (requestCode == RC_SIGN_IN) {
+                GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+                handleSignInResult(result);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        Log.d(TAG, "handleSignInResult:" + result.isSuccess());
+        if (result.isSuccess()) {
+            // Signed in successfully, show authenticated UI.
+            GoogleSignInAccount acct = result.getSignInAccount();
+
+            Log.e(TAG, "display name: " + acct.getDisplayName());
+
+            String personName = acct.getDisplayName();
+            String personPhotoUrl = acct.getPhotoUrl().toString();
+            String email = acct.getEmail();
+
+            Log.e(TAG, "Name: " + personName + ", email: " + email
+                    + ", Image: " + personPhotoUrl);
+/*
+            txtName.setText(personName);
+            txtEmail.setText(email);
+            Glide.with(getApplicationContext()).load(personPhotoUrl)
+                    .thumbnail(0.5f)
+                    .crossFade()
+                    .diskCacheStrategy(DiskCacheStrategy.ALL)
+                    .into(imgProfilePic);*/
+
+            //updateUI(true);
+        } else {
+            // Signed out, show unauthenticated UI.
+           // updateUI(false);
+        }
     }
 
 }
